dc68b3d92d6cea4b654e041a3c716c7b44df7e13
R3 <124260951+RaphaelRabeloRates@users.noreply.github.com>
2024-01-13 23:02:27 -0300

    tetandi a interface grafica

--- Pixelink/src/br/com/Pixelnik/teste/Teste.java
+++ Pixelink/src/br/com/Pixelnik/teste/Teste.java
@@ -1,10 +1,32 @@
 package br.com.Pixelnik.teste;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.WindowConstants;
+
+
 public class Teste {
 
-	public static void main(String[] args) {
-		System.out.println("helloooooooooooooooo, world");
+    public static void main(String[] args) {
+        // Criação do JFrame
+        JFrame frame = new JFrame("Minha Interface Java");
+        frame.setSize(400, 300);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
-	}
+        // Criação do JPanel
+        JPanel panel = new JPanel();
 
+        // Criação do JButton
+        JButton botao = new JButton("Clique-me");
+
+        // Adiciona o botão ao painel
+        panel.add(botao);
+
+        // Adiciona o painel ao frame
+        frame.add(panel);
+
+        // Torna o frame visível
+        frame.setVisible(true);
+    }
 }
--- Pixelink/src/module-info.java
+++ Pixelink/src/module-info.java
@@ -5,4 +5,5 @@
  * 
  */
 module Pixelink {
+	requires java.desktop;
 }
