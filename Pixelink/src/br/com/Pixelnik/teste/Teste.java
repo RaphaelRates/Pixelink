c88f0b02f31113810a75f811fb93654ddde65d8b
R3 <124260951+RaphaelRabeloRates@users.noreply.github.com>
2024-01-13 23:08:03 -0300

    testando a interface grafica do javax.swing

--- Pixelink/src/br/com/Pixelnik/teste/Teste.java
+++ Pixelink/src/br/com/Pixelnik/teste/Teste.java
@@ -1,5 +1,7 @@
 package br.com.Pixelnik.teste;
 
+import java.awt.Color;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -10,12 +12,13 @@ public class Teste {
 
     public static void main(String[] args) {
         // Criação do JFrame
-        JFrame frame = new JFrame("Minha Interface Java");
-        frame.setSize(400, 300);
+        JFrame frame = new JFrame("Pixellink - v1.0.0 ");
+        frame.setSize(1920, 1080);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
         // Criação do JPanel
         JPanel panel = new JPanel();
+        panel.setBackground(Color.MAGENTA);
 
         // Criação do JButton
         JButton botao = new JButton("Clique-me");
