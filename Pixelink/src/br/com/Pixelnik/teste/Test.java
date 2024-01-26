8e92be65a68e4f1c1b2856a7327630c83d22e6a4
R3 <124260951+RaphaelRabeloRates@users.noreply.github.com>
2024-01-26 13:40:51 -0300

    colcoand uma imagem como icone

--- Pixelink/src/br/com/Pixelnik/teste/Test.java
+++ Pixelink/src/br/com/Pixelnik/teste/Test.java
@@ -1,43 +1,21 @@
 package br.com.Pixelnik.teste;
 
-import java.awt.EventQueue;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;     
 
-import javax.swing.JFrame;
+public class Test{
 
-public class Test {
-
-	private JFrame frame;
-
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Test window = new Test();
-					window.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	/**
-	 * Create the application.
-	 */
-	public Test() {
-		initialize();
-	}
-
-	/**
-	 * Initialize the contents of the frame.
-	 */
-	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	}
-
+    public static void main(String[] args) {
+        
+    	JFrame frame = new JFrame();
+    	frame.setTitle("Pixelink");
+    	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    	frame.setSize(1920,1080);
+    	frame.setVisible(true);
+    	
+    	ImageIcon pixelIcon = new ImageIcon("IconPixelink.png");
+    	
+    	
+    }
 }
+
