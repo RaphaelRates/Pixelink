1d46bcbbe73a887eb233245bf258a9b1f1db61ba
R3 <124260951+RaphaelRabeloRates@users.noreply.github.com>
2024-01-26 14:23:21 -0300

    ajustando

--- Pixelink/src/br/com/Pixelnik/teste/Test.java
+++ Pixelink/src/br/com/Pixelnik/teste/Test.java
@@ -1,19 +1,16 @@
 package br.com.Pixelnik.teste;
-
-import javax.swing.ImageIcon;
-import javax.swing.JFrame;     
+import javax.swing.JToolBar;
+import java.awt.BorderLayout;
+  
 
 public class Test{
 
     public static void main(String[] args) {
         
-    	JFrame frame = new JFrame();
-    	frame.setTitle("Pixelink");
-    	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    	frame.setSize(1920,1080);
-    	frame.setVisible(true);
-    	
-    	ImageIcon pixelIcon = new ImageIcon("IconPixelink.png");
+    	 Myframe myframe = new Myframe();
+    	 
+    	 JToolBar toolBar = new JToolBar();
+    	 myframe.getContentPane().add(toolBar, BorderLayout.WEST);
     	
     	
     }
